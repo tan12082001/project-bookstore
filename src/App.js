4aec40f1dcaca44fd2a6e64bca1954710619e220
Tanmayi Manku <tanmayitanum@gmail.com>
2023-08-18 16:31:33 +0530

    Install react-router-dom and implement basic SPA

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import './App.css';
+import './styles/App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Books from './components/books-page';
+import Categories from './components/categories-page';
+import Navbar from './components/nav-bar';
 
 function App() {
   return (
-    <div>
-      <h1>Bookstore CMS</h1>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Books />} />
+        <Route path="/Categories" element={<Categories />} />
+      </Routes>
+    </Router>
   );
 }
 
