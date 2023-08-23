15513d2bb4560ae5619de75f9cae4040e71a5dbf
Tanmayi Manku <tanmayitanum@gmail.com>
2023-08-24 03:22:42 +0530

    use redux hooks to make use of state

--- src/components/books-list.js
+++ src/components/books-list.js
@@ -1,52 +1,17 @@
 import { useSelector } from 'react-redux';
-import { useState } from 'react';
 import EachBook from './each-book';
 import AddBook from './addbook-form';
 
 const BooksList = () => {
-  const examplebooks = [
-    {
-      id: 1,
-      title: 'Book one',
-      author: 'Author one',
-    },
-    {
-      id: 2,
-      title: 'Book two',
-      author: 'Author two',
-    },
-    {
-      id: 3,
-      title: 'Book three',
-      author: 'Author three',
-    },
-  ];
-  const [books, setBooks] = useState(examplebooks);
-  const deleteBook = (id) => {
-    const updatedBooks = books.filter((book) => book.id !== id).map((book, index) => ({
-      ...book,
-      id: index + 1,
-    }));
-    setBooks(updatedBooks);
-  };
-  const addBook = (title, author) => {
-    const newBook = {
-      id: books.length + 1,
-      title,
-      author,
-    };
-    setBooks([...books, newBook]);
-  };
-  const testbooks = useSelector((state) => state.books.ibooks);
-  console.log(testbooks);
+  const testbooks = useSelector((state) => state.books.books);
   return (
     <>
       <div className="books-list">
         {testbooks.map((book) => (
-          <EachBook key={book.id} book={book} deleteBook={deleteBook} />
+          <EachBook key={book.item_id} book={book} />
         ))}
       </div>
-      <AddBook addBook={addBook} />
+      <AddBook />
     </>
 
   );
