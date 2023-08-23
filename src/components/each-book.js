018b72c03ed433a718740fe2c3d776ec7181612b
Tanmayi Manku <tanmayitanum@gmail.com>
2023-08-24 03:24:53 +0530

    Update Button comp and use hook to use actions

--- src/components/each-book.js
+++ src/components/each-book.js
@@ -1,24 +1,29 @@
+import { useDispatch } from 'react-redux';
 import PropTypes from 'prop-types';
+import { removeBook } from '../redux/books/booksSlice';
+import Button from './button-handle';
 
-const EachBook = ({ book, deleteBook }) => (
-  <div className="book-outer">
-    <div className="book-content">
-      <h3>{book.title}</h3>
-      <p>{book.author}</p>
+const EachBook = ({ book }) => {
+  const dispatch = useDispatch();
+  return (
+    <div className="book-outer">
+      <div className="book-content">
+        <h3>{book.title}</h3>
+        <p>{book.author}</p>
+      </div>
+      <div className="book-buttons">
+        <Button className="delete-book" toAddRemove="DELETE" clickHandle={() => dispatch(removeBook(book.item_id))} />
+      </div>
     </div>
-    <div className="book-buttons">
-      <button type="button" className="delete-book" onClick={() => deleteBook(book.id)}>DELETE</button>
-    </div>
-  </div>
-);
+  );
+};
 
 EachBook.propTypes = {
   book: PropTypes.shape({
-    id: PropTypes.number.isRequired,
+    item_id: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
     author: PropTypes.string.isRequired,
   }).isRequired,
-  deleteBook: PropTypes.func.isRequired,
 };
 
 export default EachBook;
